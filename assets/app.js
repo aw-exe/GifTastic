257ba1ecbff6e440e049c99b7ddbc35ea599b12b
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2019-10-28 23:18:14 -0500

    2019-10-28: Corrected the formatting of my API key link. I knew something was wrong with my API Key when it as giving me a 403 error in DevTools.

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,7 @@ const anime = ["Cowboy Bebop", "Sailor Moon", "Pokémon", "Ghost in the Shell"];
 
 $("button").on("click", function() {
     const anime = $(this).attr("data-anime");
-    const queryURL = `https://api.giphy.com/v1/gifs/search?q=${anime}&api_key=5mbxP8vtVpkXaz1OIUCSLAtrfOVnab7J=10`;
+    const queryURL = `https://api.giphy.com/v1/gifs/search?api_key=5mbxP8vtVpkXaz1OIUCSLAtrfOVnab7J&q=${anime}&limit=10&offset=0&rating=PG&lang=en`;
     
     $.ajax({
       url: queryURL,
@@ -15,22 +15,12 @@ $("button").on("click", function() {
         const results = response.data;
 
         for (let i = 0; i < results.length; i++) {
-
           const animeDiv = $("<div>");
-
-          // Creating a paragraph tag with the result item's rating
           const p = $("<p>").text("Rating: " + results[i].rating);
-
-          // Creating and storing an image tag
           const animeImage = $("<img>");
-          // Setting the src attribute of the image to a property pulled off the result item
           animeImage.attr("src", results[i].images.fixed_height.url);
-
-          // Appending the paragraph and image tag to the animalDiv
           animeDiv.append(p);
-          animeDiv.append(animalImage);
-
-          // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+          animeDiv.append(animeImage);
           $("#gifs-appear-here").prepend(animeDiv);
         }
       });
