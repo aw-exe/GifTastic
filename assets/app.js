6e53e33fa3147b6159f2fea7b5329b18fa939c07
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2019-10-28 22:46:41 -0500

    2019-10-28: Added bootstrap and CSS styling to my index so the font, submit form and button, and layout would look nicer. Added my on click funtion to generate gifs from my API, but so far is not successful.

--- assets/app.js
+++ assets/app.js
@@ -1,75 +1,40 @@
-const anime = ["Cowboy Bebop", "Sailor Moon", "Pokemon", "Spirited Away" ];
+const anime = ["Cowboy Bebop", "Sailor Moon", "Pokémon", "Ghost in the Shell"];
 
-// displayMovieInfo function re-renders the HTML to display the appropriate content
-function displayAnimeGif() {
-
+$("button").on("click", function() {
     const anime = $(this).attr("data-anime");
     const queryURL = `https://api.giphy.com/v1/gifs/search?q=${anime}&api_key=5mbxP8vtVpkXaz1OIUCSLAtrfOVnab7J=10`;
-    // Creating an AJAX call for the specific movie button being clicked
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-
-        // Creating a div to hold the movie
-        const movieDiv = $("<div class='movie'>");
-
-        // Storing the rating data
-        const rating = response.Rated;
-
-        // Creating an element to have the rating displayed
-        const pOne = $("<p>").text("Rating: " + rating);
-
-        // Displaying the rating
-        movieDiv.append(pOne);
-
     
-        // Putting the entire movie above the previous movies
-        $("#movies-view").prepend(movieDiv);
-    });
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    
+      .then(function(response) {
+        console.log(queryURL);
+        console.log(response);
+        const results = response.data;
 
-}
+        for (let i = 0; i < results.length; i++) {
 
-// Function for displaying movie data
-function renderButtons() {
+          const animeDiv = $("<div>");
 
-    // Deleting the movies prior to adding new movies
-    // (this is necessary otherwise you will have repeat buttons)
-    $("#buttons-view").empty();
+          // Creating a paragraph tag with the result item's rating
+          const p = $("<p>").text("Rating: " + results[i].rating);
 
-    // Looping through the array of movies
-    for (let i = 0; i < movies.length; i++) {
+          // Creating and storing an image tag
+          const animeImage = $("<img>");
+          // Setting the src attribute of the image to a property pulled off the result item
+          animeImage.attr("src", results[i].images.fixed_height.url);
 
-        // Then dynamicaly generating buttons for each movie in the array
-        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-        const a = $("<button>");
-        // Adding a class of movie-btn to our button
-        a.addClass("movie-btn");
-        // Adding a data-attribute
-        a.attr("data-name", movies[i]);
-        // Providing the initial button text
-        a.text(movies[i]);
-        // Adding the button to the buttons-view div
-        $("#buttons-view").append(a);
-    }
-}
+          // Appending the paragraph and image tag to the animalDiv
+          animeDiv.append(p);
+          animeDiv.append(animalImage);
 
-// This function handles events where a movie button is clicked
-$("#add-movie").on("click", function (event) {
-    event.preventDefault();
-    // This line grabs the input from the textbox
-    const movie = $("#movie-input").val().trim();
+          // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+          $("#gifs-appear-here").prepend(animeDiv);
+        }
+      });
+  });
 
-    // Adding movie from the textbox to our array
-    movies.push(movie);
 
-    // Calling renderButtons which handles the processing of our movie array
-    renderButtons();
-});
 
-// Adding a click event listener to all elements with a class of "movie-btn"
-$(document).on("click", ".movie-btn", displayMovieInfo);
-
-// Calling the renderButtons function to display the intial buttons
-renderButtons();
-</script>
