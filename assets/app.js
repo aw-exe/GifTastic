27e29cbeadf07b8e5c017407949b3730bf910ac6
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2019-10-30 00:13:01 -0500

    Replaced bootstrap link with proper version along. Combined both containers in the html file so that my search form wouldn't appear at the bottom of the page.

--- assets/app.js
+++ assets/app.js
@@ -1,30 +1,52 @@
 const anime = ["Cowboy Bebop", "Sailor Moon", "Pokémon", "Ghost in the Shell"];
 
-$("button").on("click", function() {
+
+
+  function createButton() {
+    $("#buttons-view").empty();
+
+    for (let i = 0; i < anime.length; i++) {
+
+      const a = $("<button>");
+      a.addClass("anime-btn");
+      a.attr("data-anime", anime[i]);
+      a.text(anime[i]);
+      $("#buttons-view").append(a);
+
+    }
+  }
+
+$("button").on("click", function () {
     const anime = $(this).attr("data-anime");
     const queryURL = `https://api.giphy.com/v1/gifs/search?api_key=5mbxP8vtVpkXaz1OIUCSLAtrfOVnab7J&q=${anime}&limit=10&offset=0&rating=PG&lang=en`;
-    
+
     $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-    
-      .then(function(response) {
-        console.log(queryURL);
-        console.log(response);
-        const results = response.data;
+            url: queryURL,
+            method: "GET"
+        })
 
-        for (let i = 0; i < results.length; i++) {
-          const animeDiv = $("<div>");
-          const p = $("<p>").text("Rating: " + results[i].rating);
-          const animeImage = $("<img>");
-          animeImage.attr("src", results[i].images.fixed_height.url);
-          animeDiv.append(p);
-          animeDiv.append(animeImage);
-          $("#gifs-appear-here").prepend(animeDiv);
-        }
-      });
-  });
+        .then(function (response) {
+            console.log(queryURL);
+            console.log(response);
+            const results = response.data;
+
+            for (let i = 0; i < results.length; i++) {
+                const animeDiv = $("<div>");
+                const p = $("<p>").text("Rating: " + results[i].rating);
+                const animeImage = $("<img>");
+                animeImage.attr("src", results[i].images.fixed_height.url);
+                animeDiv.append(p);
+                animeDiv.append(animeImage);
+                $("#gifs-appear-here").prepend(animeDiv);
+            }
+        });
+});
 
 
+$("#add-gif").on("click", function (event) {
+    event.preventDefault();
+    const anime = $("#gif-input").val().trim();
+    anime.push(anime);
+    createButton();
+});
 
